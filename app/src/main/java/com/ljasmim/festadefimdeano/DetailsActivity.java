4cbfedc27a3bb7622f751e835d4fe61a879c7ee0
LeoJasmim <leojasmim@hotmail.com>
2017-08-11 20:57:30 -0300

    Criação da navegação entre activitys

--- app/src/main/java/com/ljasmim/festadefimdeano/DetailsActivity.java
+++ app/src/main/java/com/ljasmim/festadefimdeano/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.ljasmim.festadefimdeano;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
